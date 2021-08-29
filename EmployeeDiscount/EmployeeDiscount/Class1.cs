2f8ea0b65fd120fad55dca3019a43efef7d417e6
Tiabeany <tiaesanto@gmail.com>
2021-08-29 18:32:02 +0200

    Basic refactor on Class1

--- EmployeeDiscount/EmployeeDiscount/Class1.cs
+++ EmployeeDiscount/EmployeeDiscount/Class1.cs
@@ -6,60 +6,60 @@ namespace EmployeeDiscount.UI
 {
     public class Class1
     {
-        public decimal Calc(decimal amnt, int uid)
+        public decimal Calc(decimal amount, int employeeId)
         {
-            int tp = 0;
-            int y = 0;
-            int part_t = 2;
+            int type = 0;
+            int yearsEmployed = 0;
+            int partTimeType = 2;
             var connection = new SqlConnection("Data Source=SW-GBHA-TISL281;Initial Catalog=TWDC_CustDis;User Id=dev;Password=dev;");
-            tp = connection.ExecuteScalar<int>("SELECT type FROM Employees WHERE id=" + uid);
-            DateTime sd = connection.ExecuteScalar<DateTime>("SELECT start_date FROM Customers where id=" + uid);
+            type = connection.ExecuteScalar<int>("SELECT type FROM Employees WHERE id=" + employeeId);
+            DateTime employeeStartDate = connection.ExecuteScalar<DateTime>("SELECT start_date FROM Customers where id=" + employeeId);
            
-            y = DateTime.Now.Year - sd.Year;
+            yearsEmployed = DateTime.Now.Year - employeeStartDate.Year;
 
-            decimal result = 0;
-            decimal discB = (y > 5 ) ? (decimal)3 / 100 : (decimal)5 / 100;
+            decimal discountAmount = 0;
+            decimal extraDiscount = (yearsEmployed > 5) ? (decimal)3 / 100 : (decimal)5 / 100;
 
-            if (tp == 1) 
+            if (type == 1) 
             {
-                if (y > 3)
+                if (yearsEmployed > 3)
                 {
-                    result = (amnt * 10 / 100) - (decimal) 0.05 * (amnt * 10 / 100);
+                    discountAmount = (amount * 10 / 100) - (decimal) 0.05 * (amount * 10 / 100);
                 }
                 else
                 {
-                    result = (amnt * 10 / 100);
+                    discountAmount = (amount * 10 / 100);
                 }
 
 
             }
-            else if (tp == part_t)
+            else if (type == partTimeType)
             {
                 //(y > 5) ? (amnt * 10 / 100) - (decimal) 0.05 * (amnt * 10 / 100);
-                decimal disc2 = (decimal) 3 / 100; 
-                result = (y > 5) ? (amnt * 5 / 100) - disc2 * (amnt * 5 / 100) : (amnt * 5 / 100);
+                decimal secondDiscount = (decimal) 3 / 100; 
+                discountAmount = (yearsEmployed > 5) ? (amount * 5 / 100) - secondDiscount * (amount * 5 / 100) : (amount * 5 / 100);
             }
-            else if (tp == 3)
+            else if (type == 3)
             {
-                if (amnt > 30) 
+                if (amount > 30) 
                 {
-                    result = amnt * 5 / 100; 
+                    discountAmount = amount * 5 / 100; 
                 }
                 else
                 {
-                    result = amnt;
+                    discountAmount = amount;
                 }
             }
-            else if (tp == 4) 
+            else if (type == 4) 
             {
 
-                result = amnt;
+                discountAmount = amount;
             }
 
-            connection.Execute("UPDATE Employees SET discount=" + result + " WHERE id=" + uid);
+            connection.Execute("UPDATE Employees SET discount=" + discountAmount + " WHERE id=" + employeeId);
 
             //return the result
-            return result;
+            return discountAmount;
         }
     }
 }
